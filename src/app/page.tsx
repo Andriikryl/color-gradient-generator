76568d48957e032fa02b27230ea328036c568792
Andriikryl <andreyk557@gmail.com>
2023-10-19 21:21:54 +0300

    add rgb component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Angel from "@/component/angel/Angel";
 import Create from "@/component/create/Create";
 import Hero from "@/component/hero/Hero";
 import Path from "@/component/path/Path";
+import Rgb from "@/component/rgb/Rgb";
 
 export default function Home() {
   return (
@@ -9,7 +10,8 @@ export default function Home() {
       <Hero />
       <Create />
       <Path />
-      <Angel/>
+      <Angel />
+      <Rgb />
     </>
   );
 }
---
+++ src/component/rgb/Rgb.tsx
@@ -0,0 +1,112 @@
+"use client";
+import React, { useState } from "react";
+import { Container } from "../container/Container";
+import * as Slider from "@radix-ui/react-slider";
+import style from "./style.module.css";
+import clsx from "clsx";
+export default function Rgb() {
+  const [generatorVolumeRed, setGeneratorVolumeRed] = React.useState(0);
+  const [generatorVolumeGreen, setGeneratorVolumeGreen] = React.useState(0);
+  const [generatorVolumeBlue, setGeneratorVolumeBlue] = React.useState(0);
+
+  const Background = {
+    backgroundColor: `rgb(${generatorVolumeRed}, ${generatorVolumeGreen}, ${generatorVolumeBlue})`,
+  };
+
+  return (
+    <section className={style.rgb}>
+      <Container>
+        <h3 className={style.rgb__title}>rgb</h3>
+        <div className={style.rgb__box}>
+          <div className={style.flex__group}>
+            <div className={style.color__box}>
+              <div className={style.description__block}>
+                <span className={style.red}>red</span>
+                <span>{generatorVolumeRed}</span>
+              </div>
+              <form className={style.form}>
+                <Slider.Root
+                  className={style.SliderRoot}
+                  defaultValue={[generatorVolumeRed]}
+                  onValueChange={(newValues) => {
+                    setGeneratorVolumeRed(newValues[0]);
+                  }}
+                  max={250}
+                  step={1}
+                >
+                  <Slider.Track className={style.SliderTrack}>
+                    <Slider.Range className={style.SliderRange} />
+                  </Slider.Track>
+                  <Slider.Thumb
+                    className={style.SliderThumb}
+                    aria-label="Volume"
+                  ></Slider.Thumb>
+                </Slider.Root>
+              </form>
+            </div>
+            <div className={style.color__box}>
+              <div className={style.description__block}>
+                <span className={style.green}>Green</span>
+                <span>{generatorVolumeGreen}</span>
+              </div>
+              <form className={style.form}>
+                <Slider.Root
+                  className={style.SliderRoot}
+                  defaultValue={[generatorVolumeGreen]}
+                  onValueChange={(newValues) => {
+                    setGeneratorVolumeGreen(newValues[0]);
+                  }}
+                  max={250}
+                  step={1}
+                >
+                  <Slider.Track
+                    className={clsx(style.SliderTrack, style.track__green)}
+                  >
+                    <Slider.Range className={style.SliderRange} />
+                  </Slider.Track>
+                  <Slider.Thumb
+                    className={style.SliderThumb}
+                    aria-label="Volume"
+                  ></Slider.Thumb>
+                </Slider.Root>
+              </form>
+            </div>
+            <div className={style.color__box}>
+              <div className={style.description__block}>
+                <span className={style.blue}>Blue</span>
+                <span>{generatorVolumeBlue}</span>
+              </div>
+              <form className={style.form}>
+                <Slider.Root
+                  className={style.SliderRoot}
+                  defaultValue={[generatorVolumeBlue]}
+                  onValueChange={(newValues) => {
+                    setGeneratorVolumeBlue(newValues[0]);
+                  }}
+                  max={250}
+                  step={1}
+                >
+                  <Slider.Track
+                    className={clsx(style.SliderTrack, style.track__blue)}
+                  >
+                    <Slider.Range className={style.SliderRange} />
+                  </Slider.Track>
+                  <Slider.Thumb
+                    className={style.SliderThumb}
+                    aria-label="Volume"
+                  ></Slider.Thumb>
+                </Slider.Root>
+              </form>
+            </div>
+          </div>
+          <div className={style.box}>
+            <div className={style.rgb__colorBox} style={Background}></div>
+            <span className={style.color__value}>
+              {Background.backgroundColor}
+            </span>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
